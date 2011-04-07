ee9bf62f403ff9ea15905c1da141587a89380f51
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2011-04-07 09:05:26 -0600

    adding licence, setup and readme

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools.core import setup
+except ImportError:
+    from distutils.core import setup
+
+import os
+
+install_requires = [
+    'simplejson',
+]
+
+setup(
+    name = "magicembed",
+    version = "0.1",
+    url = "http://github.com/fitoria/django-magicembed",
+    licence = 'MIT',
+    description = 'Django template filter utils to render videos an thumbnails.',
+    author = 'Adolfo Fitoria',
+    author_email = 'adolfo.fitoria@gmail.com',
+    install_requires = install_requires,
+    packages = ['simplejson',] 
+    include_package_data = True,
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Licence :: OSI Approved :: MIT Licence',
+        'Programming Languaje :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
