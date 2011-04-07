5a9c4bfd78696fc63daf784afd6bb79b849371e8
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2011-04-07 08:52:41 -0600

    cambiando el nombre

--- magicembed/templatetags/magicembed.py
+++ magicembed/templatetags/magicembed.py
@@ -3,17 +3,16 @@ from cedaprode.autoembed.providers import get_provider
 register = template.Library()
 
 @register.filter
-def autoembed(value, arg=None):
+def magicembed(value, arg=None):
     '''value is the url and arg the size tuple
-    ussage: {% http://myurl.com/|autoembed:"640x480" %}'''
+    ussage: {% http://myurl.com/|magicembed:"640x480" %}'''
     arg = [int(item) for item in arg.split('x')]
     provider = get_provider(value, arg)
     return provider.render_video()
-    
 
 @register.filter
-def autothumbnail(value):
+def magicthumbnail(value):
     '''value is the url and arg the link_to another url
-    ussage: {% http://myurl.com/|autothumbnail: '/some/url' %}'''
+    ussage: {% http://myurl.com/|magicthumbnail: '/some/url' %}'''
     provider = get_provider(value)
     return provider.render_thumbnail()
