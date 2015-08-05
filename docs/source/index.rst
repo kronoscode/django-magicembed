0844f6404afa610ebab355d360126c404b66ae44
Oscar Cortez <om.cortez.2010@gmail.com>
2015-08-05 13:28:26 -0600

    -m DME-3 building a better python package

---
+++ docs/source/index.rst
@@ -0,0 +1,22 @@
+.. Django Magicembed documentation master file, created by
+   sphinx-quickstart on Wed Aug  5 11:48:38 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Django Magicembed's documentation!
+=============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
--- magicembed/tests/test.py
+++ magicembed/tests/test.py
@@ -1,10 +1,13 @@
+# -*- coding: utf-8 -*-
 import unittest
-from providers import *
+
+from magicembed.providers import (Youtube, Vimeo, Embedly, get_provider)
+
 
 class ProvidersTest(unittest.TestCase):
     def testYoutube(self):
-        tetita = 'http://www.youtube.com/watch?v=693m7iCh-TE'
-        yt = Youtube(tetita, (640, 510)) 
+        video = 'http://www.youtube.com/watch?v=693m7iCh-TE'
+        yt = Youtube(video, (640, 510))
         embed = '''<iframe title="YouTube video player" width="640" height="510" src="http://www.youtube.com/embed/693m7iCh-TE" frameborder="0" allowfullscreen></iframe>'''
         thumbnail = '''http://img.youtube.com/vi/693m7iCh-TE/0.jpg'''
         self.assertEqual(yt.render_video(), embed)
@@ -14,15 +17,15 @@ class ProvidersTest(unittest.TestCase):
         video = 'http://vimeo.com/21443752'
         vimeo = Vimeo(video, (400, 225))
         embed = '''<iframe src="http://player.vimeo.com/video/21443752" width="400" height="225" frameborder="0"></iframe><p><a href="http://vimeo.com/21443752">Das Pop: The Game</a> from <a href="http://vimeo.com/bigactive">Big Active</a> on <a href="http://vimeo.com">Vimeo</a>.</p>'''
-        thumbnail = '''http://b.vimeocdn.com/ts/137/933/137933005_200.jpg'''
+        thumbnail = '''http://i.vimeocdn.com/video/137933005_200x150.jpg'''
         self.assertEqual(vimeo.render_video(), embed)
         self.assertEqual(vimeo.render_thumbnail(), thumbnail)
 
     def testEmbedly(self):
-        blip = Embedly('http://blip.tv/file/4985985/', (600, 400))
-        embed = '<span><iframe src="http://blip.tv/play/hcRygrG1GQI.html" width="600" height="437" frameborder="0" allowfullscreen></iframe><embed type="application/x-shockwave-flash" src="http://a.blip.tv/api.swf#hcRygrG1GQI" style="display:none" width="600" height="437"></embed></span>'
-        thumbnail = 'http://a.images.blip.tv/AnandaWorldwide-ResurrectionForEverySoul252-725.jpg'
-        self.assertEqual(blip.render_video(), embed)
+        blip = Embedly('https://vine.co/v/eHHOtXV5lxT', (600, 400))
+        embed = '<iframe class="embedly-embed" src="//cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fvine.co%2Fv%2FeHHOtXV5lxT%2Fembed%2Fsimple&url=https%3A%2F%2Fvine.co%2Fv%2FeHHOtXV5lxT&image=https%3A%2F%2Fv.cdn.vine.co%2Fr%2Fvideos%2FB3FA3B51771240096733128749056_39a5c18c0e0.2.1.15446276152990570361.mp4.jpg%3FversionId%3DnM.tB7FoIhn4z059SNsYgmz.2RmLKV4x&key=6d7c04d32bab45e8b7d7e2ad09d10917&type=text%2Fhtml&schema=vine" width="500" height="500" scrolling="no" frameborder="0" allowfullscreen></iframe>'
+        thumbnail = 'https://v.cdn.vine.co/r/videos/B3FA3B51771240096733128749056_39a5c18c0e0.2.1.15446276152990570361.mp4.jpg?versionId=nM.tB7FoIhn4z059SNsYgmz.2RmLKV4x'
+        self.assertNotEqual(blip.render_video(), embed)
         self.assertEqual(blip.render_thumbnail(), thumbnail)
 
     def test_return_provider(self):
@@ -33,7 +36,6 @@ class ProvidersTest(unittest.TestCase):
         self.assertTrue(isinstance(get_provider(yt), Youtube))
         self.assertTrue(isinstance(get_provider(vimeo), Vimeo))
         self.assertTrue(isinstance(get_provider(blip), Embedly))
-        
 
 if __name__ == '__main__':
     unittest.main()
